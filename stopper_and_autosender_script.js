eeda14d5cb4ee3019a67bdeb86779dde04d0f5ff
Toremin <93540642+torem1n@users.noreply.github.com>
2023-08-31 01:03:09 +0400

    Update stopper_and_autosender_script.js

--- stopper_and_autosender_script.js
+++ stopper_and_autosender_script.js
@@ -1,3 +1,3 @@
 If You Want Script Autosender ETH/BNB/BSC And Other Coins , OR you want  Autosender Stopper/Remover   Contact: IF YOU WANT SCRIPT DM ME IN TELEGRAM 👇 👇 👇
-https://t.me/tukuno
-https://t.me/tukuno
+https://t.me/tuknoe
+https://t.me/tuknoe
